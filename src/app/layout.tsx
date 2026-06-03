c01f471bc51be59fda68ab5d38173209b8020329
abdideev <160268349+abdideev@users.noreply.github.com>
2026-06-02 18:02:17 -0600

    feat: implement responsive header and full screen menu with animations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Header } from "@/components/layout/Header";
 import { Jost, Scheherazade_New } from "next/font/google";
 import "./globals.css";
 
@@ -29,7 +30,10 @@ export default function RootLayout({
       lang="en"
       className={`${jost.variable} ${scheherazade.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
