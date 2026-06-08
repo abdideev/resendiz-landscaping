a0e256512d30b0506f4a2b3c3c607f9d7e52a72f
ivaaanDev <braulioirc@gmail.com>
2026-06-08 11:58:54 -0600

    feat(contact): centralize contact info in constants

---
+++ src/constants/contact.ts
@@ -0,0 +1,6 @@
+export const CONTACT = {
+  phone: "+15719197885",
+  phoneDisplay: "(571) 919-7885",
+  phoneHref: "tel:+15719197885",
+  whatsappNumber: "15719197885", // sin + ni símbolos, para wa.me
+} as const;
